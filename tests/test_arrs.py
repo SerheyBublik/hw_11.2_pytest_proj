8a9bd83ee32b567ef438f3827a4a39589e760985
serhey <zf.41@tut.by>
2023-07-12 22:21:26 +0300

    add @pytest.mark.parametrize

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,10 +1,15 @@
+import pytest
+
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
-    assert arrs.get([1, 2, 3], -1, "test") == "test"
+@pytest.mark.parametrize('array, index, defaul, value', [
+    ([1, 2, 3], 1, "test", 2),
+    ([], 0, "test", "test"),
+    ([1, 2, 3], -1, "test", "test")
+])
+def test_get(array, index, defaul, value):
+    assert arrs.get(array, index, defaul) == value
 
 
 def test_slice():
@@ -13,4 +18,3 @@ def test_slice():
     assert arrs.my_slice([], 1) == []
     assert arrs.my_slice([1, 2, 3], -1) == [3]
     assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
-
